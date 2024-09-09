ca266615d10f5f3840d87a5664483f9b5fffe073
Rankie <zehuajiang@u.nus.edu>
2024-09-09 13:44:32 +0800

    Update keyword_generator.py

--- keyword_generator.py
+++ keyword_generator.py
@@ -4,12 +4,9 @@ import streamlit as st
 def prompt_kw_generation(research_question, language):
 
     prompt_kw = f"""
-    ### 
-    Role
+
     You are a specialized academic professor assistant.
 
-    ### 
-    Task
     The user focuses on the research question: {research_question}. Your task is to identify highly relevant and specific keyword phrases in English, based on the provided research question. The explanations for these keyword phrases should be provided in the language specified by the input variable {language} and should be sufficiently detailed to guide effective academic research. Follow these steps to ensure comprehensive coverage:
 
     1. **Understand the Question**: Interpret the research question to grasp its core components, objectives, and the relationships between key concepts thoroughly.
@@ -23,10 +20,6 @@ def prompt_kw_generation(research_question, language):
     5. **Final Evaluation**: Select the top 3 most useful and detailed English keyword phrases and provide a comprehensive explanation for each, in the language specified by {language}.
 
     The output should be in JSON format, providing only the final three keyword phrases with their explanations, structured for clear and concise data representation. Do not include the intermediate steps. Here’s the format for your output.
-
-
-    ### 
-    Output Format
 
     ```json
     {{
@@ -50,10 +43,10 @@ def prompt_kw_generation(research_question, language):
     return prompt_kw
 
 
-def keyword_generator(research_question, language, GPT_API_KEY,model_type="gpt-4"):
+def keyword_generator(research_question, language, GPT_API_KEY,model_type="gpt-4o"):
     # 该函数会在 page 里被召唤
     os.environ["OPENAI_API_KEY"] =  GPT_API_KEY
-    with st.status("开始上传研究问题", expanded=True) as status:
+    with st.status("开始上传研究问题", expanded=True) as status2:
         st.write("问题上传中...")
     # 以下是丢给 chatGPT 生成 summary 的代码
         prompt_kw = prompt_kw_generation(research_question,language)
