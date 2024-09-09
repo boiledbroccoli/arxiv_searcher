42c412a0e70a4dc58fb99ffb163c27f14d762d1a
Rankie <zehuajiang@u.nus.edu>
2024-09-09 12:43:11 +0800

    Update summarize_all.py

--- summarize_all.py
+++ summarize_all.py
@@ -19,56 +19,47 @@ def get_focus_def(focus) -> list:
 
 def summarize_all(papers4summarise,language,role,major,question,focus,GPT_API_KEY,category = ''):
     # 该函数会在 page 里被召唤
-    with st.status("数据传递中", expanded=True) as status:
-        st.write("数据传递中...")    
-        papers4summarise = papers4summarise[['Title','Abstract']]
-        os.environ["OPENAI_API_KEY"] =  GPT_API_KEY
+     
+    papers4summarise = papers4summarise[['Title','Abstract']]
+    os.environ["OPENAI_API_KEY"] =  GPT_API_KEY
+
+    focus_def = get_focus_def(focus)
+    if len(focus) > 1:
+        focus = '、'.join(focus)
+    else:
+        focus = focus[0]
+    with open('prompt_summarize_all.txt','r') as f:
+        prompt_summarise_all   = f.read()
     
-        focus_def = get_focus_def(focus)
-        if len(focus) > 1:
-            focus = '、'.join(focus)
-        else:
-            focus = focus[0]
-        with open('prompt_summarize_all.txt','r') as f:
-            prompt_summarise_all   = f.read()
+    if category == '':
+        point4 = ''
+    else:
+        category += "的学习风格"
+        if  '直觉型' in category:
+            category_format = \
+            "   - 直觉型的学习者喜欢创新，不喜欢重复而规律的事物。\n"\
+            "   - 直觉型学习者擅长理解新的概念，对于抽象概念或数理公式容易适应，没有困扰。\n"\
+            "   - 直觉型学习者喜欢以快速而创新的方式完成工作。\n"\
+            "   - 直觉型学习者无法忍受需要填鸭式的大量记忆和冗长枯燥的计算。\n"\
+            "   - 直觉型学习者面对枯燥的内容可能会感到无趣，需要提供所学相关的说明或理论解释。"
+        elif '感受型' in category :
+            category_format = \
+            "   - 感受型的学习者偏好学习具体的事实，喜欢发觉事物间的关联或可能性\n"\
+            "   - 感受型的学习者通常喜欢用固有的方式去解决问题，不喜欢遇到复杂或是突发的状况，当课堂测验的内容超出所学的范围时，感受型学习者较 无法接受这种情况\n"\
+            "   - 感受型学习者对于细节会较有耐心去完成，而且擅长具体事实的记忆，以及实际操作的演练.\n"\
+            "   - 感受型学习者比较实际且谨慎，较无法接受所学内容和真实世界是没有关连性的。\n"\
+            "   - 感受型学习者能够发觉到学习与真实情境的关联性，这会让他们对于资讯的理解与记忆有较佳的成效。"
+        point4 = f'4. 根据{category}，使用适合该风格的语言和形式呈现总结。{category}的具体表现形式为:\n{category_format}'
+
+    prompt_summarise_all = eval('f' + repr(prompt_summarise_all))
+    print(prompt_summarise_all)
         
-        if category == '':
-            point4 = ''
-        else:
-            category += "的学习风格"
-            if  '直觉型' in category:
-                category_format = \
-                "   - 直觉型的学习者喜欢创新，不喜欢重复而规律的事物。\n"\
-                "   - 直觉型学习者擅长理解新的概念，对于抽象概念或数理公式容易适应，没有困扰。\n"\
-                "   - 直觉型学习者喜欢以快速而创新的方式完成工作。\n"\
-                "   - 直觉型学习者无法忍受需要填鸭式的大量记忆和冗长枯燥的计算。\n"\
-                "   - 直觉型学习者面对枯燥的内容可能会感到无趣，需要提供所学相关的说明或理论解释。"
-            elif '感受型' in category :
-                category_format = \
-                "   - 感受型的学习者偏好学习具体的事实，喜欢发觉事物间的关联或可能性\n"\
-                "   - 感受型的学习者通常喜欢用固有的方式去解决问题，不喜欢遇到复杂或是突发的状况，当课堂测验的内容超出所学的范围时，感受型学习者较 无法接受这种情况\n"\
-                "   - 感受型学习者对于细节会较有耐心去完成，而且擅长具体事实的记忆，以及实际操作的演练.\n"\
-                "   - 感受型学习者比较实际且谨慎，较无法接受所学内容和真实世界是没有关连性的。\n"\
-                "   - 感受型学习者能够发觉到学习与真实情境的关联性，这会让他们对于资讯的理解与记忆有较佳的成效。"
-            point4 = f'4. 根据{category}，使用适合该风格的语言和形式呈现总结。{category}的具体表现形式为:\n{category_format}'
-    
-        prompt_summarise_all = eval('f' + repr(prompt_summarise_all))
-        print(prompt_summarise_all)
-        
-        status.update(label="生成总结...")
-        st.write("生成总结...")
-        
-        client = openai.OpenAI()
-        response = client.chat.completions.create(model='gpt-4o',
-                                                  messages=[
-                                                      {"role": "user", "content": prompt_summarise_all}]) # notice
+    client = openai.OpenAI()
+    response = client.chat.completions.create(model='gpt-4o',
+                                              messages=[
+                                                  {"role": "user", "content": prompt_summarise_all}]) # notice
     
 
     summary = response.choices[0].message.content
-    status.update(label="整理总结中...", expanded=True)
-    st.write("整理总结中...")
 
-    time.sleep(1)
-
-    status.update(label="成功🎉", expanded=False)
     return summary
