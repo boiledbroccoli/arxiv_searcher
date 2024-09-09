a858581b305cb7ecf63fa4d7f8d3da970ba316ab
Rankie <zehuajiang@u.nus.edu>
2024-09-09 12:19:20 +0800

    Update page.py

--- page.py
+++ page.py
@@ -255,7 +255,7 @@ def page_search():
             st.write("选中的标题:")
             for _, selected_row in selected_rows.iterrows():
                 st.write(selected_row['Title'])
-                summary_selected = summarize_all(selected_rows[['Title','Abstract']],language,role,major,research_question,focus,GPT_API_KEY,category)
+                summary_selected = summarize_all(selected_rows,language,role,major,research_question,focus,GPT_API_KEY,category)
         else:
             st.write("没有选中的行。")
         
@@ -283,7 +283,7 @@ def page_selected():
     summary = ''
     summarise_history = st.button("总结以上文献", key = 'summarise_history')
     if summarise_history:
-        summary = summarize_all(st.session_state.history_papers[['Title','Abstract']],language,role,major,research_question,focus,GPT_API_KEY,category)
+        summary = summarize_all(st.session_state.history_papers,language,role,major,research_question,focus,GPT_API_KEY,category)
     container = st.container(border=True)
     container.write("## 文献总结")
     container.write(summary)
